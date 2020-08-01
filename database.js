6d5b11a37e5ad3ce78d08c0754d97813e08894e3
OdilonLimaNeto <odilongeronimoo@gmail.com>
2020-08-01 13:44:38 -0500

    added database configurations

---
+++ database.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: 'RlK8nu_YErc',
+    title: 'Design UX/UI, Home Office, Dev em Cidade Pequena, Portfólio, Insegurança | #PR 11',
+    duration: '13:58',
+    price: 'Free'
+  },
+
+  {
+    id: 'rCeGfFk-uCk',
+    title: 'Setup para apps Node.js (TypeScript, ESLint, Jest, Babel) | Code/Drops #33',
+    duration: '45:38',
+    price: 'Free'
+  },
+
+  {
+    id: 'eUwiTnK0EA0',
+    title: 'LIVE: Quando me tornei um Programador Profissional (A História de 16 Programadores)',
+    duration: '03:28',
+    price: 'Free'
+  },
+
+  {
+    id: '2iS3SHwft0k',
+    title: 'Prisma 2: Automatize o acesso ao banco de dados | Code/Drops #29',
+    duration: '31:54',
+    price: 'Free'
+  },
+
+  {
+    id: 'BvhYm0BOLvA',
+    title: 'Dark Mode raiz com CSS e JavaScript | Code/Drops #24',
+    duration: '17:34',
+    price: 'Free'
+  },
+
+  {
+    id: 'nhW70H9H4gU',
+    title: 'Espaçamentos e a mágica do CSS Box Model | Masterclass #10',
+    duration: '39:11',
+    price: 'Free'
+  },
+]
