c892013577d0eb2e116dbd70a80ada620f5ba47e
OdilonLimaNeto <odilongeronimoo@gmail.com>
2020-08-02 14:40:37 -0500

    added database configurations

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const database = require('./database')
 const server = express();
 
 server.use(express.static('public'));
@@ -14,14 +15,23 @@ express: server
 });
 
 
-
 // ROUTES
 server.get('/', function(request, response){
-  return response.render('about');
+
+  const about = {
+    avatarURL: '/assets/ODILON.jpeg',
+    name: 'Odilon Lima',
+    links: [
+      { name: '/assets/github.png', url: 'https://github.com/OdilonLimaNeto' },
+      { name: '/assets/facebook.png', url: 'https://www.facebook.com/profile.php?id=100009148764045' },
+      { name: '/assets/linkedIn.png', url: 'https://www.linkedin.com/in/odilonlimaneto/' }
+    ],
+  }
+  return response.render('about', { about });
 });
 
 server.get('/videos', function(request, response){
-  return response.render('videos');
+  return response.render('videos', { videos: database });
 });
 
 
