2be4f213c3be5f5dd5a53335b3841d77a14410aa
OdilonLimaNeto <odilongeronimoo@gmail.com>
2020-08-02 14:53:32 -0500

    added database configurations parte 1

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ server.set('view engine', 'njk');
 
 // CONFIGURATIONS
 nunjucks.configure("views", {
-express: server
+express: server,
+autoescape: false
 });
 
 
@@ -21,6 +22,8 @@ server.get('/', function(request, response){
   const about = {
     avatarURL: '/assets/ODILON.jpeg',
     name: 'Odilon Lima',
+    hole: 'Estudante de Sistemas de informação no <a href="https://www.uninorteac.edu.br/" target="blank">Centro universitário uninorte</a>',
+    technology: 'Entusiasta das tecnologias <a id="node" href="https://nodejs.org/en/" target="blank">Node.js</a>, <a id="react" href="https://pt-br.reactjs.org/" target="blank">React</a> e <a id="react-native" href="https://reactnative.dev/" target="blank">React Native</a>',
     links: [
       { name: '/assets/github.png', url: 'https://github.com/OdilonLimaNeto' },
       { name: '/assets/facebook.png', url: 'https://www.facebook.com/profile.php?id=100009148764045' },
@@ -28,7 +31,6 @@ server.get('/', function(request, response){
     ],
   }
   return response.render('about', { about });
-  
 });
 
 server.get('/videos', function(request, response){
